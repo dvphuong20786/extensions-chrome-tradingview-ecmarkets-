b10c45a0e6248ec2c7539368e99fe798ba78f233
phuongdv <dvphuong.dev@gmail.com>
2025-11-17 12:26:51 +0700

    Update quanlyaccount_cm_markets.js

--- scripts/quanlyaccount_cm_markets.js
+++ scripts/quanlyaccount_cm_markets.js
@@ -45,7 +45,7 @@ $(window).load(function (e) {
 });
 
  
-var list_exclude = ["11009398", "11011359"];
+var list_exclude = ["11009398", "11011359", "11011527"];
 var count = 60;
 var g_accounts = [];
 //----------------AUTO FORM IP ADDRESS-----------------
